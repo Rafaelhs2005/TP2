9c39b3ba4d9f04c3359f806277f2aa87e90e579a
Rafael Henrique Silva <rhenriques.2005@gmail.com>
2025-04-11 14:38:02 -0300

    socorro

--- c2.c
+++ c2.c
@@ -3,46 +3,35 @@
 #include <string.h>
 #include <stdbool.h>
 
-#define MAX_LINE 8192
-#define MAX_FIELDS 12
-#define MAX_ITEMS 50
-#define MAX_STR 256
+#define MAX_SHOWS 10000
+#define MAX_LINE 1000
+#define MAX_FIELD 200
 
 typedef struct {
-    char showId[MAX_STR];
-    char type[MAX_STR];
-    char title[MAX_STR];
-    char director[MAX_ITEMS][MAX_STR];
+    char showId[MAX_FIELD];
+    char type[MAX_FIELD];
+    char title[MAX_FIELD];
+    char director[10][MAX_FIELD];
     int directorCount;
-    char cast[MAX_ITEMS][MAX_STR];
+    char cast[20][MAX_FIELD];
     int castCount;
-    char country[MAX_STR];
-    char dateAdded[MAX_STR];
+    char country[MAX_FIELD];
+    char dateAdded[MAX_FIELD];
     int releaseYear;
-    char rating[MAX_STR];
-    char duration[MAX_STR];
-    char listedIn[MAX_ITEMS][MAX_STR];
-    int listedInCount;
+    char rating[MAX_FIELD];
+    char duration[MAX_FIELD];
+    char listedIn[10][MAX_FIELD];
+    int listedCount;
 } Show;
 
-void trim(char *str) {
-    char *end;
-    while (*str == ' ' || *str == '\t' || *str == '\n' || *str == '\r') str++;
-    if (*str == 0) return;
-    end = str + strlen(str) - 1;
-    while (end > str && (*end == ' ' || *end == '\t' || *end == '\n' || *end == '\r')) end--;
-    *(end + 1) = 0;
-}
-
-int contarAspas(const char *linha) {
+int contarAspas(const char* linha) {
     int count = 0;
-    for (int i = 0; linha[i] != '\0'; i++) {
+    for (int i = 0; linha[i]; i++)
         if (linha[i] == '"') count++;
-    }
     return count;
 }
 
-char* lerLinhaCompleta(FILE *fp, char *buffer) {
+char* lerLinhaCompleta(FILE* fp, char* buffer) {
     char temp[MAX_LINE];
     buffer[0] = '\0';
     int aspas = 0;
@@ -56,121 +45,152 @@ char* lerLinhaCompleta(FILE *fp, char *buffer) {
     return strlen(buffer) == 0 ? NULL : buffer;
 }
 
-void splitCSV(char *linha, char campos[MAX_FIELDS][MAX_LINE]) {
-    int i = 0, j = 0, k = 0;
-    bool dentroAspas = false;
-    for (; linha[i] != '\0'; i++) {
+int splitCSV(const char* linha, char campos[][MAX_FIELD]) {
+    int campoIndex = 0, i = 0, j = 0;
+    bool aspas = false;
+    char buffer[MAX_FIELD] = "";
+
+    while (linha[i]) {
         if (linha[i] == '"') {
-            dentroAspas = !dentroAspas;
-        } else if (linha[i] == ',' && !dentroAspas) {
-            campos[j][k] = '\0';
-            j++;
-            k = 0;
+            aspas = !aspas;
+        } else if (linha[i] == ',' && !aspas) {
+            buffer[j] = '\0';
+            strcpy(campos[campoIndex++], j == 0 ? "NaN" : buffer);
+            j = 0;
         } else {
-            if (k < MAX_LINE - 1) campos[j][k++] = linha[i];
+            buffer[j++] = linha[i];
         }
+        i++;
     }
-    campos[j][k] = '\0';
-    for (int x = j + 1; x < MAX_FIELDS; x++) campos[x][0] = '\0';
+
+    buffer[j] = '\0';
+    strcpy(campos[campoIndex++], j == 0 ? "NaN" : buffer);
+
+    return campoIndex;
 }
 
-int compararStrings(const void *a, const void *b) {
-    const char *sa = *(const char **)a;
-    const char *sb = *(const char **)b;
-    return strcmp(sa, sb);
+void trim(char* str) {
+    int len = strlen(str);
+    while (len > 0 && (str[len-1] == '\n' || str[len-1] == '\r'))
+        str[--len] = '\0';
 }
 
-void separarLista(char *campo, char destino[MAX_ITEMS][MAX_STR], int *count) {
-    if (strcmp(campo, "NaN") == 0) {
-        strncpy(destino[0], "NaN", MAX_STR);
-        *count = 1;
+void parseArray(char* campo, char arr[][MAX_FIELD], int* count) {
+    *count = 0;
+    if (strcmp(campo, "NaN") == 0) return;
+
+    char* token = strtok(campo, ",");
+    while (token != NULL && *count < 20) {
+        while (*token == ' ') token++;
+        strcpy(arr[(*count)++], token);
+        token = strtok(NULL, ",");
+    }
+}
+
+void lerShow(Show* s, const char* linha) {
+    char campos[11][MAX_FIELD];
+    char linhaCopia[MAX_LINE];
+    strcpy(linhaCopia, linha);
+
+    int qtd = splitCSV(linhaCopia, campos);
+    if (qtd < 11) {
+        printf("Erro: Linha com campos insuficientes\n");
         return;
     }
 
-    char *token = strtok(campo, ",");
-    *count = 0;
-    while (token && *count < MAX_ITEMS) {
-        trim(token);
-        strncpy(destino[*count], token, MAX_STR - 1);
-        destino[*count][MAX_STR - 1] = '\0';
-        (*count)++;
-        token = strtok(NULL, ",");
-    }
+    strcpy(s->showId, campos[0]);
+    strcpy(s->type, campos[1]);
+    strcpy(s->title, campos[2]);
 
-    char *ordenar[MAX_ITEMS];
-    for (int i = 0; i < *count; i++) ordenar[i] = destino[i];
-    qsort(ordenar, *count, sizeof(char*), compararStrings);
-    for (int i = 0; i < *count; i++) strncpy(destino[i], ordenar[i], MAX_STR);
+    parseArray(campos[3], s->director, &s->directorCount);
+    parseArray(campos[4], s->cast, &s->castCount);
+
+    strcpy(s->country, strcmp(campos[5], "NaN") == 0 ? "NaN" : campos[5]);
+    strcpy(s->dateAdded, strcmp(campos[6], "NaN") == 0 ? "March 1, 1900" : campos[6]);
+
+    s->releaseYear = strcmp(campos[7], "NaN") == 0 ? 0 : atoi(campos[7]);
+    strcpy(s->rating, campos[8]);
+    strcpy(s->duration, campos[9]);
+
+    parseArray(campos[10], s->listedIn, &s->listedCount);
 }
 
-void lerShow(Show *s, char campos[MAX_FIELDS][MAX_LINE]) {
-    strncpy(s->showId, campos[0][0] ? campos[0] : "NaN", MAX_STR);
-    strncpy(s->type, campos[1][0] ? campos[1] : "NaN", MAX_STR);
-    strncpy(s->title, campos[2][0] ? campos[2] : "NaN", MAX_STR);
-
-    separarLista(campos[3][0] ? campos[3] : "NaN", s->director, &s->directorCount);
-    separarLista(campos[4][0] ? campos[4] : "NaN", s->cast, &s->castCount);
-
-    strncpy(s->country, campos[5][0] ? campos[5] : "NaN", MAX_STR);
-    strncpy(s->dateAdded, campos[6][0] ? campos[6] : "March 1, 1900", MAX_STR);
-    s->releaseYear = campos[7][0] ? atoi(campos[7]) : 0;
-    strncpy(s->rating, campos[8][0] ? campos[8] : "NaN", MAX_STR);
-    strncpy(s->duration, campos[9][0] ? campos[9] : "NaN", MAX_STR);
-    separarLista(campos[10][0] ? campos[10] : "NaN", s->listedIn, &s->listedInCount);
-}
-
-void imprimirShow(const Show *s) {
+void imprimirShow(const Show* s) {
     printf("=> %s ## %s ## %s ## ", s->showId, s->title, s->type);
-    for (int i = 0; i < s->directorCount; i++) {
-        printf("%s%s", s->director[i], (i < s->directorCount - 1 ? ", " : ""));
+
+    if (s->directorCount > 0) {
+        for (int i = 0; i < s->directorCount; i++) {
+            printf("%s", s->director[i]);
+            if (i < s->directorCount - 1) printf(", ");
+        }
+    } else {
+        printf("NaN");
     }
-    printf(" ## [");
-    for (int i = 0; i < s->castCount; i++) {
-        printf("%s%s", s->cast[i], (i < s->castCount - 1 ? ", " : ""));
+    printf(" ## ");
+
+    if (s->castCount > 0) {
+        printf("[");
+        for (int i = 0; i < s->castCount; i++) {
+            printf("%s", s->cast[i]);
+            if (i < s->castCount - 1) printf(", ");
+        }
+        printf("]");
+    } else {
+        printf("NaN");
     }
-    printf("] ## %s ## %s ## %d ## %s ## %s ## [", s->country, s->dateAdded, s->releaseYear, s->rating, s->duration);
-    for (int i = 0; i < s->listedInCount; i++) {
-        printf("%s%s", s->listedIn[i], (i < s->listedInCount - 1 ? ", " : ""));
+    printf(" ## ");
+
+    printf("%s ## %s ## %d ## %s ## %s ## ", s->country, s->dateAdded, s->releaseYear, s->rating, s->duration);
+
+    if (s->listedCount > 0) {
+        printf("[");
+        for (int i = 0; i < s->listedCount; i++) {
+            printf("%s", s->listedIn[i]);
+            if (i < s->listedCount - 1) printf(", ");
+        }
+        printf("]");
+    } else {
+        printf("NaN");
     }
-    printf("] ##\n");
+    printf(" ##\n");
 }
 
 int main() {
-    FILE *fp = fopen("/tmp/disneyplus.csv", "r");
+    FILE* fp = fopen("/tmp/disneyplus.csv", "r");
     if (!fp) {
-        perror("Erro ao abrir o arquivo");
+        printf("Erro ao abrir o arquivo.\n");
         return 1;
     }
 
-    char linhaCompleta[MAX_LINE];
-    char campos[MAX_FIELDS][MAX_LINE];
-    Show shows[10000];
-    int totalShows = 0;
+    char linha[MAX_LINE];
+    fgets(linha, MAX_LINE, fp); // Pula cabeçalho
 
-    fgets(linhaCompleta, MAX_LINE, fp);
-    while (lerLinhaCompleta(fp, linhaCompleta)) {
-        splitCSV(linhaCompleta, campos);
-        lerShow(&shows[totalShows++], campos);
+    Show shows[MAX_SHOWS];
+    int count = 0;
+
+    while (lerLinhaCompleta(fp, linha) != NULL && count < MAX_SHOWS) {
+        lerShow(&shows[count++], linha);
     }
     fclose(fp);
 
-    char entrada[MAX_STR];
-    printf("Digite um ID de show (ou FIM para sair):\n");
-    while (scanf("%255s", entrada) == 1) {
-        if (strcmp(entrada, "FIM") == 0) break;
+    char input[MAX_FIELD];
+    while (true) {
+        fgets(input, MAX_FIELD, stdin);
+        trim(input);
+        if (strcmp(input, "FIM") == 0) break;
 
         bool encontrado = false;
-        for (int i = 0; i < totalShows; i++) {
-            if (strcmp(shows[i].showId, entrada) == 0) {
+        for (int i = 0; i < count; i++) {
+            if (strcmp(shows[i].showId, input) == 0) {
                 imprimirShow(&shows[i]);
                 encontrado = true;
                 break;
             }
         }
+
         if (!encontrado) {
-            printf("Show com ID \"%s\" nao encontrado.\n", entrada);
+            printf("Show com ID \"%s\" não encontrado.\n", input);
         }
-        printf("\nDigite outro ID de show (ou FIM para sair):\n");
     }
 
     return 0;
